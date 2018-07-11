9226de4ed509c28e6eb665abaf214834c6703784
Raphael Randschau <nicolai86@me.com>
2018-07-10 22:13:13 -0700

    add remote description to STUN example

--- examples/stun/main.go
+++ examples/stun/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"fmt"
+	"os"
 
 	"github.com/pions/webrtc"
+	"github.com/pions/webrtc/pkg/ice"
+	"github.com/pions/webrtc/pkg/rtp"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	rawSd, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("")
+	sd, err := base64.StdEncoding.DecodeString(rawSd)
+	if err != nil {
+		panic(err)
+	}
+
 	peerConnection, err := webrtc.New(&webrtc.RTCConfiguration{
 		ICEServers: []webrtc.RTCICEServer{
 			{
@@ -16,6 +32,18 @@ func main() {
 		},
 	})
 	if err != nil {
+		panic(err)
+	}
+
+	peerConnection.Ontrack = func(mediaType webrtc.TrackType, packets <-chan *rtp.Packet) {
+		fmt.Printf("Got a %s track\n", mediaType)
+	}
+
+	peerConnection.OnICEConnectionStateChange = func(connectionState ice.ConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	}
+
+	if err := peerConnection.SetRemoteDescription(string(sd)); err != nil {
 		panic(err)
 	}
 
