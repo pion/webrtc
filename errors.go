c85cded582fa2ba0a6303d0f692a8c51de2c9233
backkem <mail@backkem.me>
2018-08-02 12:12:14 +0200

    API: Initial api structure for creating datachannel and sctp information.

--- errors.go
+++ errors.go
@@ -91,5 +91,33 @@ func (e *SyntaxError) Error() string {
 	return fmt.Sprintf("syntax error: %v", e.Err)
 }
 
+// Types of TypeError
+var (
+	ErrInvalidValue = errors.New("invalid value")
+)
+
+// TypeError indicates an issue with a supplied value
+type TypeError struct {
+	Err error
+}
+
+func (e *SyntaxError) Error() string {
+	return fmt.Sprintf("type error: %v", e.Err)
+}
+
+// Types of OperationError
+var (
+	ErrMaxDataChannels = errors.New("maximum number of datachannels reached")
+)
+
+// OperationError indicates an issue with execution
+type OperationError struct {
+	Err error
+}
+
+func (e *SyntaxError) Error() string {
+	return fmt.Sprintf("operation error: %v", e.Err)
+}
+
 // ErrUnknownType indicates a Unknown info
 var ErrUnknownType = errors.New("Unknown")
