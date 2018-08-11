3e67d71290d5e60b778bf28b055f8e48ed17570b
Sean DuBois <sean@siobud.com>
2018-08-11 00:55:12 -0700

    Update panic lint script with more functions

---
+++ .github/lint-disallowed-functions-in-library.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Disallow usages of functions that cause the program to exit in the library code
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+EXCLUDE_DIRECTORIES="--exclude-dir=examples --exclude-dir=.git --exclude-dir=.github "
+DISALLOWED_FUNCTIONS=('os.Exit(', 'panic(', 'Fatal(', 'Fatalf(', 'Fatalln(')
+
+
+for disallowedFunction in "${DISALLOWED_FUNCTIONS[@]}"
+do
+	if grep -R $EXCLUDE_DIRECTORIES -e "$disallowedFunction" "$SCRIPT_PATH/.." | grep -v nolint; then
+		echo "$disallowedFunction may only be used in example code"
+		exit 1
+	fi
+done
