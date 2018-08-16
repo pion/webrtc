845c009b97fe2667d34bce0d3964213988d1a474
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-08-16 02:39:10 -0400

    Move ICE package from public to internal folder structure

--- internal/sdp/ice.go
+++ internal/sdp/ice.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/pions/webrtc/pkg/ice"
+	"github.com/pions/webrtc/internal/ice"
 )
 
 // ICECandidateUnmarshal takes a candidate strings and returns a ice.Candidate or nil if it fails to parse
