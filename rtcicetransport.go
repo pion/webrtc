57baa20575aefd6bae043b325d1d4134d3df07e1
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-09-05 16:28:39 -0400

    Add RTCIceTransport commented out public API

--- rtcicetransport.go
+++ rtcicetransport.go
@@ -3,4 +3,28 @@ package webrtc
 // RTCIceTransport allows an application access to information about the ICE
 // transport over which packets are sent and received.
 type RTCIceTransport struct {
+	// Role RTCIceRole
+	// Component RTCIceComponent
+	// State RTCIceTransportState
+	// gatheringState RTCIceGathererState
 }
+
+// func (t *RTCIceTransport) GetLocalCandidates() []RTCIceCandidate {
+//
+// }
+//
+// func (t *RTCIceTransport) GetRemoteCandidates() []RTCIceCandidate {
+//
+// }
+//
+// func (t *RTCIceTransport) GetSelectedCandidatePair() RTCIceCandidatePair {
+//
+// }
+//
+// func (t *RTCIceTransport) GetLocalParameters() RTCIceParameters {
+//
+// }
+//
+// func (t *RTCIceTransport) GetRemoteParameters() RTCIceParameters {
+//
+// }
