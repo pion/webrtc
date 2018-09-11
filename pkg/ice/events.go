2bda887e12ed0c3fd2a937d9ed7f6bd2a7945ced
Konstantin Itskov <konstantin.itskov@kovits.com>
2018-09-11 15:37:25 -0400

    Add documentations for ice.ReceiveEvent structure

--- pkg/ice/events.go
+++ pkg/ice/events.go
@@ -1,7 +1,13 @@
 package ice
 
+// ReceiveEvent represents the data invoked on the ice.OnReceive method.
 type ReceiveEvent struct {
+	// Buffer represents the raw data received from the agent.
 	Buffer []byte
-	Local  string
+
+	// Local represents the local transport address which handled the packet.
+	Local string
+
+	// Remote represents the remote transport address which sent the packet.
 	Remote string
 }
