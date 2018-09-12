4ca153f6f869a813a8a7a0dd50be4b06cfe7a2f5
Max Hawkins <maxhawkins@gmail.com>
2018-09-12 13:36:53 -0700

    RTCP: add test that parses a real packet
    Parses RR & SDES packets. Not done yet, but getting closer...
    
    Relates to #119

--- pkg/rtcp/header_test.go
+++ pkg/rtcp/header_test.go
@@ -5,31 +5,6 @@ import (
 	"testing"
 )
 
-// An RTCP packet from a packet dump
-var realPacket = []byte{129, 201, 0, 7, 144, 47, 158, 46, 188, 94, 154, 64, 0, 0, 0, 0, 0, 0, 70, 225, 0, 0, 1, 17, 9, 243, 100, 50, 0, 2, 74, 121, 129, 202, 0, 12, 144, 47, 158, 46, 1, 38, 123, 57, 99, 48, 48, 101, 98, 57, 50, 45, 49, 97, 102, 98, 45, 57, 100, 52, 57, 45, 97, 52, 55, 100, 45, 57, 49, 102, 54, 52, 101, 101, 101, 54, 57, 102, 53, 125, 0, 0, 0, 0, 129, 203, 0, 1, 144, 47, 158, 46}
-
-func TestHeaderUnmarshal(t *testing.T) {
-	data := make([]byte, headerLength)
-	copy(data, realPacket)
-
-	want := Header{
-		Version:     2,
-		Padding:     false,
-		ReportCount: 1,
-		Type:        TypeReceiverReport,
-		Length:      7,
-	}
-
-	var got Header
-	if err := got.Unmarshal(data); err != nil {
-		t.Errorf("Unmarshal: %v", err)
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Unmarshal: got %#v, want %#v", got, want)
-	}
-}
-
 func TestHeaderUnmarshalNil(t *testing.T) {
 	var header Header
 	err := header.Unmarshal(nil)
