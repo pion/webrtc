af7d64a1643cb3b2d9542353a3d74c51f94b91c4
backkem <mail@backkem.me>
2018-09-28 18:36:28 +0200

    Examples: make pion-to-pion cross platform
    Resolves #185

--- examples/pion-to-pion/README.md
+++ examples/pion-to-pion/README.md
@@ -1,7 +1,19 @@
 # pion-to-pion
-pion-to-pion is an example of two pion instances communicating directly
+pion-to-pion is an example of two pion instances communicating directly!
 
-This example is NOT complete, it will finish ICE but nothing will happen after
+The SDP offer and answer are exchanged automatically over HTTP.
+The `answer` side acts like a HTTP server and should therefore be ran first.
 
 ## Instructions
-TODO
+First run `answer`:
+```sh
+go install github.com/pions/webrtc/examples/pion-to-pion/answer
+answer
+```
+Next, run `offer`:
+```sh
+go install github.com/pions/webrtc/examples/pion-to-pion/offer
+offer
+```
+
+You should see them connect and start to exchange messages.
--- examples/pion-to-pion/answer/main.go
+++ examples/pion-to-pion/answer/main.go
@@ -1,98 +1,132 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
+	"math/rand"
+	"net/http"
+	"time"
 
 	"github.com/pions/webrtc"
 	"github.com/pions/webrtc/pkg/datachannel"
 	"github.com/pions/webrtc/pkg/ice"
 )
 
-func buildPeerConnection() *webrtc.RTCPeerConnection {
-	peerConnection, err := webrtc.New(webrtc.RTCConfiguration{
+func main() {
+	addr := flag.String("address", ":50000", "Address to host the HTTP server on.")
+	flag.Parse()
+
+	offerChan, answerChan := mustSignalViaHTTP(*addr)
+
+	/* Everything below is the pion-WebRTC API, thanks for using it! */
+
+	// Prepare the configuration
+	config := webrtc.RTCConfiguration{
 		IceServers: []webrtc.RTCIceServer{
 			{
 				URLs: []string{"stun:stun.l.google.com:19302"},
 			},
 		},
-	})
-	if err != nil {
-		panic(err)
 	}
 
-	d, err := peerConnection.CreateDataChannel("data", nil)
-	if err != nil {
-		panic(err)
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.New(config)
+	check(err)
+
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange = func(connectionState ice.ConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
 	}
 
-	d.Lock()
-	d.Onmessage = func(payload datachannel.Payload) {
-		switch p := payload.(type) {
-		case *datachannel.PayloadString:
-			fmt.Printf("Message '%s' from DataChannel '%s' payload '%s'\n", p.PayloadType().String(), d.Label, string(p.Data))
-		case *datachannel.PayloadBinary:
-			fmt.Printf("Message '%s' from DataChannel '%s' payload '% 02x'\n", p.PayloadType().String(), d.Label, p.Data)
-		default:
-			fmt.Printf("Message '%s' from DataChannel '%s' no payload \n", p.PayloadType().String(), d.Label)
+	// Register data channel creation handling
+	peerConnection.OnDataChannel = func(d *webrtc.RTCDataChannel) {
+		fmt.Printf("New DataChannel %s %d\n", d.Label, d.ID)
+
+		d.Lock()
+		defer d.Unlock()
+
+		// Register channel opening handling
+		d.OnOpen = func() {
+			fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label, d.ID)
+
+			for range time.NewTicker(5 * time.Second).C {
+				message := randSeq(15)
+				fmt.Printf("Sending %s \n", message)
+
+				err := d.Send(datachannel.PayloadString{Data: []byte(message)})
+				check(err)
+			}
+		}
+
+		// Register message handling
+		d.Onmessage = func(payload datachannel.Payload) {
+			switch p := payload.(type) {
+			case *datachannel.PayloadString:
+				fmt.Printf("Message '%s' from DataChannel '%s' payload '%s'\n", p.PayloadType().String(), d.Label, string(p.Data))
+			case *datachannel.PayloadBinary:
+				fmt.Printf("Message '%s' from DataChannel '%s' payload '% 02x'\n", p.PayloadType().String(), d.Label, p.Data)
+			default:
+				fmt.Printf("Message '%s' from DataChannel '%s' no payload \n", p.PayloadType().String(), d.Label)
+			}
 		}
 	}
-	d.Unlock()
 
-	peerConnection.OnICEConnectionStateChange = func(connectionState ice.ConnectionState) {
-		fmt.Printf("Connection State has changed %s \n", connectionState.String())
-	}
+	// Wait for the remote SessionDescription
+	offer := <-offerChan
 
-	return peerConnection
+	err = peerConnection.SetRemoteDescription(offer)
+	check(err)
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	answer, err := peerConnection.CreateAnswer(nil)
+	check(err)
+
+	// Send the answer
+	answerChan <- answer
+
+	// Block forever
+	select {}
 }
 
-func main() {
-	ln, err := net.Listen("unix", "../pion.sock")
-	if err != nil {
-		log.Fatal("Failed to listen: ", err)
+// mustSignalViaHTTP exchange the SDP offer and answer using an HTTP server.
+func mustSignalViaHTTP(address string) (offerOut chan webrtc.RTCSessionDescription, answerIn chan webrtc.RTCSessionDescription) {
+	offerOut = make(chan webrtc.RTCSessionDescription)
+	answerIn = make(chan webrtc.RTCSessionDescription)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var offer webrtc.RTCSessionDescription
+		err := json.NewDecoder(r.Body).Decode(&offer)
+		check(err)
+
+		offerOut <- offer
+		answer := <-answerIn
+
+		err = json.NewEncoder(w).Encode(answer)
+		check(err)
+
+	})
+
+	go http.ListenAndServe(address, nil)
+	fmt.Println("Listening on", address)
+
+	return
+}
+
+func randSeq(n int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[r.Intn(len(letters))]
 	}
+	return string(b)
+}
 
-	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
-	go func(ln net.Listener, c chan os.Signal) {
-		sig := <-c
-		log.Printf("Caught signal %s: shutting down.", sig)
-		ln.Close()
-		os.Exit(0)
-	}(ln, sigc)
-
-	fmt.Println("Ready for offer")
-	fd, err := ln.Accept()
+// check is used to panic in an error occurs.
+func check(err error) {
 	if err != nil {
-		log.Fatal("Accept error: ", err)
-	}
-
-	buf := make([]byte, 5000)
-	n, err := fd.Read(buf)
-	if err != nil {
-		return
-	}
-
-	peerConnection := buildPeerConnection()
-
-	if err := peerConnection.SetRemoteDescription(webrtc.RTCSessionDescription{
-		Type: webrtc.RTCSdpTypeOffer,
-		Sdp:  string(buf[0:n]),
-	}); err != nil {
 		panic(err)
 	}
-
-	answer, err := peerConnection.CreateAnswer(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	if _, err := fd.Write([]byte(answer.Sdp)); err != nil {
-		log.Fatal("Writing client error: ", err)
-	}
-	select {}
 }
