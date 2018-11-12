a29eec6765d16a90cd734aed3a8b5663fe7ad6a3
Michael MacDonald <github@macdonald.cx>
2018-11-12 15:09:00 -0500

    Implement OnSignalingStateChange
    Resolves #226

--- rtcsignalingstate.go
+++ rtcsignalingstate.go
@@ -1,5 +1,30 @@
 package webrtc
 
+import (
+	"fmt"
+
+	"github.com/pions/webrtc/pkg/rtcerr"
+	"github.com/pkg/errors"
+)
+
+type rtcStateChangeOp int
+
+const (
+	rtcStateChangeOpSetLocal rtcStateChangeOp = iota + 1
+	rtcStateChangeOpSetRemote
+)
+
+func (op rtcStateChangeOp) String() string {
+	switch op {
+	case rtcStateChangeOpSetLocal:
+		return "SetLocal"
+	case rtcStateChangeOpSetRemote:
+		return "SetRemote"
+	default:
+		return "Unknown State Change Operation"
+	}
+}
+
 // RTCSignalingState indicates the signaling state of the offer/answer process.
 type RTCSignalingState int
 
@@ -76,5 +101,79 @@ func (t RTCSignalingState) String() string {
 		return rtcSignalingStateClosedStr
 	default:
 		return ErrUnknownType.Error()
+	}
+}
+
+func checkNextSignalingState(cur, next RTCSignalingState, op rtcStateChangeOp, sdpType RTCSdpType) (RTCSignalingState, error) {
+	// Special case for rollbacks
+	if sdpType == RTCSdpTypeRollback && cur == RTCSignalingStateStable {
+		return cur, &rtcerr.InvalidModificationError{
+			Err: errors.New("Can't rollback from stable state"),
+		}
+	}
+
+	// 4.3.1 valid state transitions
+	switch cur {
+	case RTCSignalingStateStable:
+		switch op {
+		case rtcStateChangeOpSetLocal:
+			// stable->SetLocal(offer)->have-local-offer
+			if sdpType == RTCSdpTypeOffer && next == RTCSignalingStateHaveLocalOffer {
+				return next, nil
+			}
+		case rtcStateChangeOpSetRemote:
+			// stable->SetRemote(offer)->have-remote-offer
+			if sdpType == RTCSdpTypeOffer && next == RTCSignalingStateHaveRemoteOffer {
+				return next, nil
+			}
+		}
+	case RTCSignalingStateHaveLocalOffer:
+		if op == rtcStateChangeOpSetRemote {
+			switch sdpType {
+			// have-local-offer->SetRemote(answer)->stable
+			case RTCSdpTypeAnswer:
+				if next == RTCSignalingStateStable {
+					return next, nil
+				}
+			// have-local-offer->SetRemote(pranswer)->have-remote-pranswer
+			case RTCSdpTypePranswer:
+				if next == RTCSignalingStateHaveRemotePranswer {
+					return next, nil
+				}
+			}
+		}
+	case RTCSignalingStateHaveRemotePranswer:
+		if op == rtcStateChangeOpSetRemote && sdpType == RTCSdpTypeAnswer {
+			// have-remote-pranswer->SetRemote(answer)->stable
+			if next == RTCSignalingStateStable {
+				return next, nil
+			}
+		}
+	case RTCSignalingStateHaveRemoteOffer:
+		if op == rtcStateChangeOpSetLocal {
+			switch sdpType {
+			// have-remote-offer->SetLocal(answer)->stable
+			case RTCSdpTypeAnswer:
+				if next == RTCSignalingStateStable {
+					return next, nil
+				}
+			// have-remote-offer->SetLocal(pranswer)->have-local-pranswer
+			case RTCSdpTypePranswer:
+				if next == RTCSignalingStateHaveLocalPranswer {
+					return next, nil
+				}
+			}
+		}
+	case RTCSignalingStateHaveLocalPranswer:
+		if op == rtcStateChangeOpSetLocal && sdpType == RTCSdpTypeAnswer {
+			// have-local-pranswer->SetLocal(answer)->stable
+			if next == RTCSignalingStateStable {
+				return next, nil
+			}
+		}
+	}
+
+	return cur, &rtcerr.InvalidModificationError{
+		Err: fmt.Errorf("Invalid proposed signaling state transition %s->%s(%s)->%s", cur, op, sdpType, next),
 	}
 }
