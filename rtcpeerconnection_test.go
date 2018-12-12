97f538a6f2e7ca820a379ac6e026283d3b3b7b2c
backkem <mail@backkem.me>
2018-12-12 02:11:55 +0100

    Add PeerConnection Close test
    Relates to #276

--- rtcpeerconnection_test.go
+++ rtcpeerconnection_test.go
@@ -6,6 +6,9 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"math/big"
+	"os"
+	"runtime"
+	"runtime/pprof"
 	"testing"
 	"time"
 
@@ -420,4 +423,90 @@ func TestRTCPeerConnection_EventHandlers(t *testing.T) {
 		<-onICEConnectionStateChangeCalled,
 		<-onDataChannelCalled,
 	}))
+}
+
+func TestRTCPeerConnection_Close(t *testing.T) {
+	expectedGoRoutineCount := runtime.NumGoroutine()
+
+	pcOffer, pcAnswer, err := newPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	awaitSetup := make(chan struct{})
+	pcAnswer.OnDataChannel(func(d *RTCDataChannel) {
+		close(awaitSetup)
+	})
+
+	_, err = pcOffer.CreateDataChannel("data", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = signalPair(pcOffer, pcAnswer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	<-awaitSetup
+
+	err = pcOffer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = pcAnswer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkGoRoutineCount(t, expectedGoRoutineCount)
+}
+
+func newPair() (pcOffer *RTCPeerConnection, pcAnswer *RTCPeerConnection, err error) {
+	pca, err := New(RTCConfiguration{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pcb, err := New(RTCConfiguration{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return pca, pcb, nil
+}
+
+func signalPair(pcOffer *RTCPeerConnection, pcAnswer *RTCPeerConnection) error {
+	offer, err := pcOffer.CreateOffer(nil)
+	if err != nil {
+		return err
+	}
+
+	err = pcAnswer.SetRemoteDescription(offer)
+	if err != nil {
+		return err
+	}
+
+	answer, err := pcAnswer.CreateAnswer(nil)
+	if err != nil {
+		return err
+	}
+
+	err = pcOffer.SetRemoteDescription(answer)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkGoRoutineCount(t *testing.T, expectedGoRoutineCount int) {
+	goRoutineCount := runtime.NumGoroutine()
+	if goRoutineCount != expectedGoRoutineCount {
+		if err := pprof.Lookup("goroutine").WriteTo(os.Stderr, 1); err != nil {
+			t.Fatal(err)
+		}
+		t.Fatalf("goRoutineCount != expectedGoRoutineCount, possible leak: %d %d", goRoutineCount, expectedGoRoutineCount)
+	}
 }
