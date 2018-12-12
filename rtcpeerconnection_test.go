d380a31a063d575bb4a1cbacbe0a0d7fc7eb89d7
backkem <mail@backkem.me>
2018-12-12 22:42:34 +0100

    PC: Move close test to separate file
    Relates to #276

--- rtcpeerconnection_test.go
+++ rtcpeerconnection_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/pions/transport/test"
 	"github.com/pions/webrtc/pkg/ice"
 
 	"github.com/pions/webrtc/pkg/media"
@@ -421,79 +420,4 @@ func TestRTCPeerConnection_EventHandlers(t *testing.T) {
 		<-onICEConnectionStateChangeCalled,
 		<-onDataChannelCalled,
 	}))
-}
-
-func TestRTCPeerConnection_Close(t *testing.T) {
-	report := test.CheckRoutines(t)
-	defer report()
-
-	pcOffer, pcAnswer, err := newPair()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	awaitSetup := make(chan struct{})
-	pcAnswer.OnDataChannel(func(d *RTCDataChannel) {
-		close(awaitSetup)
-	})
-
-	_, err = pcOffer.CreateDataChannel("data", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = signalPair(pcOffer, pcAnswer)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	<-awaitSetup
-
-	err = pcOffer.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = pcAnswer.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func newPair() (pcOffer *RTCPeerConnection, pcAnswer *RTCPeerConnection, err error) {
-	pca, err := New(RTCConfiguration{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	pcb, err := New(RTCConfiguration{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return pca, pcb, nil
-}
-
-func signalPair(pcOffer *RTCPeerConnection, pcAnswer *RTCPeerConnection) error {
-	offer, err := pcOffer.CreateOffer(nil)
-	if err != nil {
-		return err
-	}
-
-	err = pcAnswer.SetRemoteDescription(offer)
-	if err != nil {
-		return err
-	}
-
-	answer, err := pcAnswer.CreateAnswer(nil)
-	if err != nil {
-		return err
-	}
-
-	err = pcOffer.SetRemoteDescription(answer)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
