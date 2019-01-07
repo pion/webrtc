5fabedb155ea5bdeb6208b92ff59f71489c7ac2b
backkem <mail@backkem.me>
2019-01-07 21:02:19 +0100

    Quic: Add QuicTransport and E2E test
    Relates to #275

--- pkg/quic/transport.go
+++ pkg/quic/transport.go
@@ -2,11 +2,43 @@
 // https://w3c.github.io/webrtc-quic/
 package quic
 
+import (
+	"github.com/pions/webrtc/pkg/quic/internal/wrapper"
+)
+
 // Transport is a quic transport focused on client/server use cases.
 type Transport struct {
 	TransportBase
 }
 
 // NewTransport creates a new Transport
-// func NewTransport(url string) (*Transport, error) {
-// }
+func NewTransport(url string, config *Config) (*Transport, error) {
+	cfg := config.clone()
+	cfg.SkipVerify = true // Using self signed certificates for now
+
+	s, err := wrapper.Dial(url, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	t := &Transport{}
+	return t, t.TransportBase.startBase(s)
+}
+
+func newServer(url string, config *Config) (*Transport, error) {
+	cfg := config.clone()
+	cfg.SkipVerify = true // Using self signed certificates for now
+
+	l, err := wrapper.Listen(url, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := l.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	t := &Transport{}
+	return t, t.TransportBase.startBase(s)
+}
