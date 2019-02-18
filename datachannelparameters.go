0e9e1b0c485a681cb47cff9a3ba86425391798dd
Yutaka Takeda <yt0916@gmail.com>
2019-02-17 18:55:57 -0800

    Update sctp and datachannel revs
    Resolves #334

--- datachannelparameters.go
+++ datachannelparameters.go
@@ -2,6 +2,9 @@ package webrtc
 
 // DataChannelParameters describes the configuration of the DataChannel.
 type DataChannelParameters struct {
-	Label string `json:"label"`
-	ID    uint16 `json:"id"`
+	Label             string  `json:"label"`
+	ID                uint16  `json:"id"`
+	Ordered           bool    `json:"ordered"`
+	MaxPacketLifeTime *uint16 `json:"maxPacketLifeTime"`
+	MaxRetransmits    *uint16 `json:"maxRetransmits"`
 }
