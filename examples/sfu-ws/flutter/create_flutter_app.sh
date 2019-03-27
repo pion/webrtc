743cc14af178e494a035cbe005db181d55b292d1
湖北捷智云技术有限公司 <duanweiwei1982@gmail.com>
2019-03-27 16:09:56 +0800

    [Flutter] Delete unused files.

--- examples/sfu-ws/flutter/create_flutter_app.sh
+++
@@ -1,5 +0,0 @@
-#/!/bin/bash
-flutter create --project-name flutter_pions_sfu_ws .
-cp app_config/AndroidManifest.xml android/app/src/main/
-cp app_config/Info.plist ios/Runner/
-cp app_config/Podfile ios/
