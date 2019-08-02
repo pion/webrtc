16281c0b9edce646c6bc8773135a6655afd0770e
Atsushi Watanabe <atsushi.w@ieee.org>
2019-08-02 18:59:10 +0900

    Add ICEGatherer test case
    Add a new test for ill-order of OnLocalCandidate.
    Before this fix, nil is sent before sending all candidates
    8-10 times per 10 tries on my local environment.

--- icegatherer_test.go
+++ icegatherer_test.go
@@ -60,3 +60,57 @@ func TestNewICEGatherer_Success(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestICEGather_LocalCandidateOrder(t *testing.T) {
+	// Limit runtime in case of deadlocks
+	lim := test.TimeOut(time.Second * 20)
+	defer lim.Stop()
+
+	opts := ICEGatherOptions{
+		ICEServers: []ICEServer{{URLs: []string{"stun:stun.l.google.com:19302"}}},
+	}
+
+	to := time.Second
+	gatherer, err := NewICEGatherer(10000, 10010, &to, &to, &to, &to, &to, &to, &to, logging.NewDefaultLoggerFactory(), false, []NetworkType{NetworkTypeUDP4}, opts)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if gatherer.State() != ICEGathererStateNew {
+		t.Fatalf("Expected gathering state new")
+	}
+
+	for i := 0; i < 10; i++ {
+		candidate := make(chan *ICECandidate)
+		gatherer.OnLocalCandidate(func(c *ICECandidate) {
+			candidate <- c
+		})
+
+		if err := gatherer.SignalCandidates(); err != nil {
+			t.Error(err)
+		}
+		endGathering := false
+
+	L:
+		for {
+			select {
+			case c := <-candidate:
+				if c == nil {
+					endGathering = true
+				} else if endGathering {
+					t.Error("Received a candidate after the last candidate")
+					break L
+				}
+			case <-time.After(100 * time.Millisecond):
+				if !endGathering {
+					t.Error("Timed out before receiving the last candidate")
+				}
+				break L
+			}
+		}
+	}
+
+	if err := gatherer.Close(); err != nil {
+		t.Error(err)
+	}
+}
