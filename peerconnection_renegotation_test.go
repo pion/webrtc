512a7d33305b4b00adf9ff22d0d3f656cd895771
Sean DuBois <seaduboi@amazon.com>
2020-02-08 01:52:30 -0800

    Implement RemoveTrack
    RemoveTrack properly removes the Sender, and culls
    the RtpReceiver on the remote
    
    Relates to #207

--- peerconnection_renegotation_test.go
+++ peerconnection_renegotation_test.go
@@ -4,13 +4,26 @@ package webrtc
 
 import (
 	"context"
+	"io"
 	"math/rand"
 	"testing"
 	"time"
 
+	"github.com/pion/transport/test"
 	"github.com/pion/webrtc/v2/pkg/media"
 	"github.com/stretchr/testify/assert"
 )
+
+func sendVideoUntilDone(c context.Context, t *testing.T, track *Track) {
+	for {
+		select {
+		case <-time.After(20 * time.Millisecond):
+			assert.NoError(t, track.WriteSample(media.Sample{Data: []byte{0x00}, Samples: 1}))
+		case <-c.Done():
+			return
+		}
+	}
+}
 
 /*
 *  Assert the following behaviors
@@ -19,17 +32,12 @@ import (
 * - We are able to re-negotiate and AddTrack is properly called
  */
 func TestPeerConnection_Renegotation_AddTrack(t *testing.T) {
-	const (
-		expectedTrackID    = "video"
-		expectedTrackLabel = "pion"
-	)
-
 	api := NewAPI()
-	// lim := test.TimeOut(time.Second * 30)
-	// defer lim.Stop()
+	lim := test.TimeOut(time.Second * 30)
+	defer lim.Stop()
 
-	// report := test.CheckRoutines(t)
-	// defer report()
+	report := test.CheckRoutines(t)
+	defer report()
 
 	api.mediaEngine.RegisterDefaultCodecs()
 	pcOffer, pcAnswer, err := api.newPair()
@@ -46,20 +54,12 @@ func TestPeerConnection_Renegotation_AddTrack(t *testing.T) {
 		onTrackFiredFunc()
 	})
 
-	haveConnected, haveConnectedFunc := context.WithCancel(context.Background())
-	pcOffer.OnICEConnectionStateChange(func(i ICEConnectionState) {
-		if i == ICEConnectionStateConnected {
-			haveConnectedFunc()
-		}
-	})
-
 	assert.NoError(t, signalPair(pcOffer, pcAnswer))
-	<-haveConnected.Done()
 
 	_, err = pcAnswer.AddTransceiverFromKind(RTPCodecTypeVideo, RtpTransceiverInit{Direction: RTPTransceiverDirectionRecvonly})
 	assert.NoError(t, err)
 
-	vp8Track, err := pcOffer.NewTrack(DefaultPayloadTypeVP8, rand.Uint32(), expectedTrackID, expectedTrackLabel)
+	vp8Track, err := pcOffer.NewTrack(DefaultPayloadTypeVP8, rand.Uint32(), "foo", "bar")
 	assert.NoError(t, err)
 
 	_, err = pcOffer.AddTrack(vp8Track)
@@ -74,17 +74,56 @@ func TestPeerConnection_Renegotation_AddTrack(t *testing.T) {
 	haveRenegotiated.set(true)
 	assert.NoError(t, signalPair(pcOffer, pcAnswer))
 
-	func() {
+	sendVideoUntilDone(onTrackFired, t, vp8Track)
+
+	assert.NoError(t, pcOffer.Close())
+	assert.NoError(t, pcAnswer.Close())
+}
+
+func TestPeerConnection_Renegotation_RemoveTrack(t *testing.T) {
+	api := NewAPI()
+	lim := test.TimeOut(time.Second * 30)
+	defer lim.Stop()
+
+	report := test.CheckRoutines(t)
+	defer report()
+
+	api.mediaEngine.RegisterDefaultCodecs()
+	pcOffer, pcAnswer, err := api.newPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = pcAnswer.AddTransceiverFromKind(RTPCodecTypeVideo, RtpTransceiverInit{Direction: RTPTransceiverDirectionRecvonly})
+	assert.NoError(t, err)
+
+	vp8Track, err := pcOffer.NewTrack(DefaultPayloadTypeVP8, rand.Uint32(), "foo", "bar")
+	assert.NoError(t, err)
+
+	rtpSender, err := pcOffer.AddTrack(vp8Track)
+	assert.NoError(t, err)
+
+	onTrackFired, onTrackFiredFunc := context.WithCancel(context.Background())
+	trackClosed, trackClosedFunc := context.WithCancel(context.Background())
+
+	pcAnswer.OnTrack(func(track *Track, r *RTPReceiver) {
+		onTrackFiredFunc()
+
 		for {
-			select {
-			case <-time.After(20 * time.Millisecond):
-				assert.NoError(t, vp8Track.WriteSample(media.Sample{Data: []byte{0x00}, Samples: 1}))
-			case <-onTrackFired.Done():
+			if _, err := track.ReadRTP(); err == io.EOF {
+				trackClosedFunc()
 				return
 			}
 		}
-	}()
+	})
 
+	assert.NoError(t, signalPair(pcOffer, pcAnswer))
+	sendVideoUntilDone(onTrackFired, t, vp8Track)
+
+	assert.NoError(t, pcOffer.RemoveTrack(rtpSender))
+	assert.NoError(t, signalPair(pcOffer, pcAnswer))
+
+	<-trackClosed.Done()
 	assert.NoError(t, pcOffer.Close())
 	assert.NoError(t, pcAnswer.Close())
 }
