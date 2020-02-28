59653f9ca25e7889bd0ff90fcacd025d0b745197
Sean DuBois <seaduboi@amazon.com>
2020-02-27 16:48:29 -0800

    Put application media section first
    When offering always put application first. Before during re-negotation
    we would break the order if media sections because we would always place
    it last. Instead of remembering what order the application was place
    just always place it first.
    
    Resolves #1055

--- peerconnection_renegotation_test.go
+++ peerconnection_renegotation_test.go
@@ -127,3 +127,24 @@ func TestPeerConnection_Renegotation_RemoveTrack(t *testing.T) {
 	assert.NoError(t, pcOffer.Close())
 	assert.NoError(t, pcAnswer.Close())
 }
+
+// When creating an offer the first media section MUST be SCTP
+// This is to make renegotation easier, instead of having to remember where it
+// is was interleaved
+func TestPeerConnection_Renegotation_ApplicationFirst(t *testing.T) {
+	pc, err := NewPeerConnection(Configuration{})
+	assert.NoError(t, err)
+
+	addTransceiverAndAssert := func() {
+		_, err = pc.AddTransceiverFromKind(RTPCodecTypeVideo)
+		assert.NoError(t, err)
+
+		offer, err := pc.CreateOffer(nil)
+		assert.NoError(t, err)
+
+		assert.Equal(t, offer.parsed.MediaDescriptions[0].MediaName.Media, "application")
+	}
+
+	addTransceiverAndAssert()
+	addTransceiverAndAssert()
+}
