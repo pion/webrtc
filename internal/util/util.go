975a63fb43402129427e2a1bcd209a8628d617a1
Atsushi Watanabe <atsushi.w@ieee.org>
2020-03-23 14:58:41 +0900

    Make FlattenErrs result comparable
    Support Go 1.13 error.Is.

--- internal/util/util.go
+++ internal/util/util.go
@@ -2,7 +2,6 @@
 package util
 
 import (
-	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -21,17 +20,46 @@ func RandSeq(n int) string {
 
 // FlattenErrs flattens multiple errors into one
 func FlattenErrs(errs []error) error {
+	errs2 := []error{}
+	for _, e := range errs {
+		if e != nil {
+			errs2 = append(errs2, e)
+		}
+	}
+	if len(errs2) == 0 {
+		return nil
+	}
+	return multiError(errs2)
+}
+
+type multiError []error
+
+func (me multiError) Error() string {
 	var errstrings []string
 
-	for _, err := range errs {
+	for _, err := range me {
 		if err != nil {
 			errstrings = append(errstrings, err.Error())
 		}
 	}
 
 	if len(errstrings) == 0 {
-		return nil
+		return "multiError must contain multiple error but is empty"
 	}
 
-	return fmt.Errorf(strings.Join(errstrings, "\n"))
+	return strings.Join(errstrings, "\n")
+}
+
+func (me multiError) Is(err error) bool {
+	for _, e := range me {
+		if e == err {
+			return true
+		}
+		if me2, ok := e.(multiError); ok {
+			if me2.Is(err) {
+				return true
+			}
+		}
+	}
+	return false
 }
