b4c0f667f463df2bbcd77ce8e00077f694da6cf2
Pion <59523206+pionbot@users.noreply.github.com>
2020-04-30 05:49:08 +0000

    Update CI configs to v0.3.0
    Update lint scripts and CI configs.
    Set repository specific GO_JS_WASM_EXEC config.

--- .github/assert-contributors.sh
+++ .github/assert-contributors.sh
@@ -16,11 +16,16 @@ fi
 
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-EXCLUDED_CONTIBUTORS=('John R. Bradley' 'renovate[bot]' 'Renovate Bot' 'Pion Bot')
-MISSING_CONTIBUTORS=()
+if [ -f ${SCRIPT_PATH}/.ci.conf ]
+then
+  . ${SCRIPT_PATH}/.ci.conf
+fi
+
+EXCLUDED_CONTRIBUTORS+=('John R. Bradley' 'renovate[bot]' 'Renovate Bot' 'Pion Bot')
+MISSING_CONTRIBUTORS=()
 
 shouldBeIncluded () {
-	for i in "${EXCLUDED_CONTIBUTORS[@]}"
+	for i in "${EXCLUDED_CONTRIBUTORS[@]}"
 	do
 		if [ "$i" == "$1" ] ; then
 			return 1
@@ -35,15 +40,15 @@ for contributor in $(git log --format='%aN' | sort -u)
 do
 	if shouldBeIncluded $contributor; then
 		if ! grep -q "$contributor" "$SCRIPT_PATH/../README.md"; then
-			MISSING_CONTIBUTORS+=("$contributor")
+			MISSING_CONTRIBUTORS+=("$contributor")
 		fi
 	fi
 done
 unset IFS
 
-if [ ${#MISSING_CONTIBUTORS[@]} -ne 0 ]; then
+if [ ${#MISSING_CONTRIBUTORS[@]} -ne 0 ]; then
     echo "Please add the following contributors to the README"
-    for i in "${MISSING_CONTIBUTORS[@]}"
+    for i in "${MISSING_CONTRIBUTORS[@]}"
     do
 	    echo "$i"
     done
