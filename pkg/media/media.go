3ebc820b03c19baaec474b8de1b8e62dc481608b
Josh Bleecher Snyder <josharian@gmail.com>
2020-05-18 15:35:05 -0700

    Add add NSamples to pkg/media
    media.Sample is hard to use. A more natural formulation would
    be in terms of a time.Duration. See discussion at
    https://gophers.slack.com/archives/CAK2124AG/p1589839755436500.
    
    That change could occur in v3. In the meantime, add a helper
    function to calculate the number of samples given a time.Duration
    and a sampling frequency, and refer to it from the Sample docs.

--- pkg/media/media.go
+++ pkg/media/media.go
@@ -2,13 +2,22 @@
 package media
 
 import (
+	"time"
+
 	"github.com/pion/rtp"
 )
 
-// Sample contains media, and the amount of samples in it
+// A Sample contains encoded media and the number of samples in that media (see NSamples).
 type Sample struct {
 	Data    []byte
 	Samples uint32
+}
+
+// NSamples calculates the number of samples in media of length d with sampling frequency f.
+// For example, NSamples(20 * time.Millisecond, 48000) will return the number of samples
+// in a 20 millisecond segment of Opus audio recorded at 48000 samples per second.
+func NSamples(d time.Duration, freq int) uint32 {
+	return uint32(time.Duration(freq) * d / time.Second)
 }
 
 // Writer defines an interface to handle
---
+++ pkg/media/media_test.go
@@ -0,0 +1,13 @@
+package media_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pion/webrtc/v2/pkg/media"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNSamples(t *testing.T) {
+	assert.Equal(t, media.NSamples(20*time.Millisecond, 48000), uint32(48000*0.02))
+}
