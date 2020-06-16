cc8d80178c80a87f66000eb59ef3282f7bb11b01
John Berthels <john.berthels@gmail.com>
2020-06-15 22:50:29 -0700

    Set role when actpass remote to DTLSRoleClient
    This matches rfc5763, setup:active is RECOMMENDED
    
    Relates to #494

--- dtlsrole.go
+++ dtlsrole.go
@@ -21,8 +21,22 @@ const (
 )
 
 const (
-	defaultDtlsRoleAnswer = DTLSRoleServer
-	defaultDtlsRoleOffer  = DTLSRoleAuto
+	// https://tools.ietf.org/html/rfc5763
+	/*
+		The answerer MUST use either a
+		setup attribute value of setup:active or setup:passive.  Note that
+		if the answerer uses setup:passive, then the DTLS handshake will
+		not begin until the answerer is received, which adds additional
+		latency. setup:active allows the answer and the DTLS handshake to
+		occur in parallel.  Thus, setup:active is RECOMMENDED.
+	*/
+	defaultDtlsRoleAnswer = DTLSRoleClient
+	/*
+		The endpoint that is the offerer MUST use the setup attribute
+		value of setup:actpass and be prepared to receive a client_hello
+		before it receives the answer.
+	*/
+	defaultDtlsRoleOffer = DTLSRoleAuto
 )
 
 func (r DTLSRole) String() string {
