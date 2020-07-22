978dd73c1fd7fd60d8b66b1a8ccb9d34e9691903
Simone Gotti <simone.gotti@gmail.com>
2020-07-22 16:20:04 -0700

    Add simulcast example
    Add jsfiddle and documentation around using the example

--- examples/README.md
+++ examples/README.md
@@ -16,6 +16,7 @@ For more full featured examples that use 3rd party libraries see our **[example-
 * [Broadcast](broadcast): The broadcast example demonstrates how to broadcast a video to multiple peers. A broadcaster uploads the video once and the server forwards it to all other peers.
 * [RTP Forwarder](rtp-forwarder): The rtp-forwarder example demonstrates how to forward your audio/video streams using RTP.
 * [RTP to WebRTC](rtp-to-webrtc): The rtp-to-webrtc example demonstrates how to take RTP packets sent to a Pion process into your browser.
+* [Simulcast](simulcast): The simulcast example demonstrates how to accept and demux 1 Track that contains 3 Simulcast streams. It then returns the media as 3 independent Tracks back to the sender.
 
 #### Data Channel API
 * [Data Channels](data-channels): The data-channels example shows how you can send/recv DataChannel messages from a web browser.
---
+++ examples/simulcast/jsfiddle/demo.js
@@ -0,0 +1,92 @@
+// Create peer conn
+const pc = new RTCPeerConnection({
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+});
+
+pc.oniceconnectionstatechange = (e) => {
+  console.log("connection state change", pc.iceConnectionState);
+};
+pc.onicecandidate = (event) => {
+  if (event.candidate === null) {
+    document.getElementById("localSessionDescription").value = btoa(
+      JSON.stringify(pc.localDescription)
+    );
+  }
+};
+
+pc.onnegotiationneeded = (e) =>
+  pc
+    .createOffer()
+    .then((d) => pc.setLocalDescription(d))
+    .catch(console.error);
+
+pc.ontrack = (event) => {
+  console.log("Got track event", event);
+  let video = document.createElement("video");
+  video.srcObject = event.streams[0];
+  video.autoplay = true;
+  video.width = "500";
+  let label = document.createElement("div");
+  label.textContent = event.streams[0].id;
+  document.getElementById("serverVideos").appendChild(label);
+  document.getElementById("serverVideos").appendChild(video);
+};
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: {
+      width: {
+        ideal: 4096,
+      },
+      height: {
+        ideal: 2160,
+      },
+      frameRate: {
+        ideal: 60,
+        min: 10,
+      },
+    },
+    audio: false,
+  })
+  .then((stream) => {
+    document.getElementById("browserVideo").srcObject = stream;
+    pc.addTransceiver(stream.getVideoTracks()[0], {
+      direction: "sendonly",
+      streams: [stream],
+      sendEncodings: [
+        // for firefox order matters... first high resolution, then scaled resolutions...
+        {
+          rid: "f",
+        },
+        {
+          rid: "h",
+          scaleResolutionDownBy: 2.0,
+        },
+        {
+          rid: "q",
+          scaleResolutionDownBy: 4.0,
+        },
+      ],
+    });
+    pc.addTransceiver("video");
+    pc.addTransceiver("video");
+    pc.addTransceiver("video");
+  });
+
+window.startSession = () => {
+  const sd = document.getElementById("remoteSessionDescription").value;
+  if (sd === "") {
+    return alert("Session Description must not be empty");
+  }
+
+  try {
+    console.log("answer", JSON.parse(atob(sd)));
+    pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))));
+  } catch (e) {
+    alert(e);
+  }
+};
