051a10edf3a0ce982cc3e9950a185ea7660a1592
Hanjun Kim <hallazzang@gmail.com>
2020-09-24 16:24:44 +0900

    add Unwrap method for error types
    Fixes #1443

--- pkg/rtcerr/errors.go
+++ pkg/rtcerr/errors.go
@@ -15,6 +15,10 @@ func (e *UnknownError) Error() string {
 	return fmt.Sprintf("UnknownError: %v", e.Err)
 }
 
+func (e *UnknownError) Unwrap() error {
+	return e.Err
+}
+
 // InvalidStateError indicates the object is in an invalid state.
 type InvalidStateError struct {
 	Err error
@@ -22,6 +26,10 @@ type InvalidStateError struct {
 
 func (e *InvalidStateError) Error() string {
 	return fmt.Sprintf("InvalidStateError: %v", e.Err)
+}
+
+func (e *InvalidStateError) Unwrap() error {
+	return e.Err
 }
 
 // InvalidAccessError indicates the object does not support the operation or
@@ -34,6 +42,10 @@ func (e *InvalidAccessError) Error() string {
 	return fmt.Sprintf("InvalidAccessError: %v", e.Err)
 }
 
+func (e *InvalidAccessError) Unwrap() error {
+	return e.Err
+}
+
 // NotSupportedError indicates the operation is not supported.
 type NotSupportedError struct {
 	Err error
@@ -41,6 +53,10 @@ type NotSupportedError struct {
 
 func (e *NotSupportedError) Error() string {
 	return fmt.Sprintf("NotSupportedError: %v", e.Err)
+}
+
+func (e *NotSupportedError) Unwrap() error {
+	return e.Err
 }
 
 // InvalidModificationError indicates the object cannot be modified in this way.
@@ -52,6 +68,10 @@ func (e *InvalidModificationError) Error() string {
 	return fmt.Sprintf("InvalidModificationError: %v", e.Err)
 }
 
+func (e *InvalidModificationError) Unwrap() error {
+	return e.Err
+}
+
 // SyntaxError indicates the string did not match the expected pattern.
 type SyntaxError struct {
 	Err error
@@ -61,6 +81,10 @@ func (e *SyntaxError) Error() string {
 	return fmt.Sprintf("SyntaxError: %v", e.Err)
 }
 
+func (e *SyntaxError) Unwrap() error {
+	return e.Err
+}
+
 // TypeError indicates an error when a value is not of the expected type.
 type TypeError struct {
 	Err error
@@ -68,6 +92,10 @@ type TypeError struct {
 
 func (e *TypeError) Error() string {
 	return fmt.Sprintf("TypeError: %v", e.Err)
+}
+
+func (e *TypeError) Unwrap() error {
+	return e.Err
 }
 
 // OperationError indicates the operation failed for an operation-specific
@@ -80,6 +108,10 @@ func (e *OperationError) Error() string {
 	return fmt.Sprintf("OperationError: %v", e.Err)
 }
 
+func (e *OperationError) Unwrap() error {
+	return e.Err
+}
+
 // NotReadableError indicates the input/output read operation failed.
 type NotReadableError struct {
 	Err error
@@ -87,6 +119,10 @@ type NotReadableError struct {
 
 func (e *NotReadableError) Error() string {
 	return fmt.Sprintf("NotReadableError: %v", e.Err)
+}
+
+func (e *NotReadableError) Unwrap() error {
+	return e.Err
 }
 
 // RangeError indicates an error when a value is not in the set or range
@@ -97,4 +133,8 @@ type RangeError struct {
 
 func (e *RangeError) Error() string {
 	return fmt.Sprintf("RangeError: %v", e.Err)
+}
+
+func (e *RangeError) Unwrap() error {
+	return e.Err
 }
