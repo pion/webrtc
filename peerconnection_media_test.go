16407672bde2f1dbc7f4e9f196473ce9d09d95cd
Juliusz Chroboczek <jch@irif.fr>
2021-02-02 22:32:16 +0100

    Avoid infinite loop in CreateOffer
    If the local description keeps getting changed, or in case of a but
    in Pion, CreateOffer never terminates, which could cause client software
    to hang.  Set an arbitrary bound on the number of iterations.
    
    Relates to #1656

--- peerconnection_media_test.go
+++ peerconnection_media_test.go
@@ -1069,3 +1069,31 @@ func TestPeerConnection_Simulcast_Probe(t *testing.T) {
 	assert.NoError(t, offerer.Close())
 	close(testFinished)
 }
+
+// Assert that CreateOffer can't enter infinite loop
+// We attempt to generate an offer multiple times in case a user
+// has edited the PeerConnection. We can assert this broken behavior with an
+// empty MediaEngine. See pion/webrtc#1656 for full behavior
+func TestPeerConnection_CreateOffer_InfiniteLoop(t *testing.T) {
+	lim := test.TimeOut(time.Second * 30)
+	defer lim.Stop()
+
+	report := test.CheckRoutines(t)
+	defer report()
+
+	m := &MediaEngine{}
+
+	pc, err := NewAPI(WithMediaEngine(m)).NewPeerConnection(Configuration{})
+	assert.NoError(t, err)
+
+	track, err := NewTrackLocalStaticRTP(RTPCodecCapability{MimeType: "video/vp8"}, "video", "pion")
+	assert.NoError(t, err)
+
+	_, err = pc.AddTrack(track)
+	assert.NoError(t, err)
+
+	_, err = pc.CreateOffer(nil)
+	assert.Error(t, err, errExcessiveRetries)
+
+	assert.NoError(t, pc.Close())
+}
