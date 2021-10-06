658e0f631246fbab462ad8a697fd3fc48c7c1ddb
Will Forcey <wsforc3y@gmail.com>
2021-10-06 15:38:34 -0400

    Add files via upload

--- peerconnection.go
+++ peerconnection.go
@@ -1616,6 +1616,15 @@ func (pc *PeerConnection) GetTransceivers() []*RTPTransceiver {
 	return pc.rtpTransceivers
 }
 
+// GetSCTPTransport returns the SCTPTransport currently attached to this PeerConnection
+func (pc *PeerConnection) GetSCTPTransport() []*RTPTransceiver {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+
+	return pc.sctpTransport
+}
+
+
 // AddTrack adds a Track to the PeerConnection
 func (pc *PeerConnection) AddTrack(track TrackLocal) (*RTPSender, error) {
 	if pc.isClosed.get() {
