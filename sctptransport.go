04fa0338487fedceb1593768f85a7aa96eb5404a
Sean DuBois <sean@siobud.com>
2021-11-05 13:44:43 -0400

    Open all DataChannels before accepting remote
    A race existed with Negotiated DataChannels before this. Before
    the remote could sends us a DataChannel message before the
    datachannel was negotiated locally. We would then discard the
    message and print an error.
    
    This PR moves the processing of remote datachannel messages after
    we have registered all local ones. It also informs
    datachannel.Accept of existing datachannels in order to prevent
    any initialization logic from proceeding incorrectly.

--- sctptransport.go
+++ sctptransport.go
@@ -110,10 +110,26 @@ func (r *SCTPTransport) Start(remoteCaps SCTPCapabilities) error {
 	}
 
 	r.lock.Lock()
-	defer r.lock.Unlock()
-
 	r.sctpAssociation = sctpAssociation
 	r.state = SCTPTransportStateConnected
+	dataChannels := append([]*DataChannel{}, r.dataChannels...)
+	r.lock.Unlock()
+
+	var openedDCCount uint32
+	for _, d := range dataChannels {
+		if d.ReadyState() == DataChannelStateConnecting {
+			err := d.open(r)
+			if err != nil {
+				r.log.Warnf("failed to open data channel: %s", err)
+				continue
+			}
+			openedDCCount++
+		}
+	}
+
+	r.lock.Lock()
+	r.dataChannelsOpened += openedDCCount
+	r.lock.Unlock()
 
 	go r.acceptDataChannels(sctpAssociation)
 
@@ -139,16 +155,34 @@ func (r *SCTPTransport) Stop() error {
 }
 
 func (r *SCTPTransport) acceptDataChannels(a *sctp.Association) {
+	r.lock.RLock()
+	dataChannels := make([]*datachannel.DataChannel, 0, len(r.dataChannels))
+	for _, dc := range r.dataChannels {
+		dc.mu.Lock()
+		isNil := dc.dataChannel == nil
+		dc.mu.Unlock()
+		if isNil {
+			continue
+		}
+		dataChannels = append(dataChannels, dc.dataChannel)
+	}
+	r.lock.RUnlock()
+ACCEPT:
 	for {
 		dc, err := datachannel.Accept(a, &datachannel.Config{
 			LoggerFactory: r.api.settingEngine.LoggerFactory,
-		})
+		}, dataChannels...)
 		if err != nil {
 			if err != io.EOF {
 				r.log.Errorf("Failed to accept data channel: %v", err)
 				r.onError(err)
 			}
 			return
+		}
+		for _, ch := range dataChannels {
+			if ch.StreamIdentifier() == dc.StreamIdentifier() {
+				continue ACCEPT
+			}
 		}
 
 		var (
