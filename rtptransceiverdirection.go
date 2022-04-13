c8b962255fb21662f2e240c435730f921a53f01e
david.s <david.s@hpcnt.com>
2022-04-13 17:23:18 +0900

    Fix RTPTransceiverDirection comments
    Missing `the` in constants comments

--- rtptransceiverdirection.go
+++ rtptransceiverdirection.go
@@ -5,19 +5,19 @@ type RTPTransceiverDirection int
 
 const (
 	// RTPTransceiverDirectionSendrecv indicates the RTPSender will offer
-	// to send RTP and RTPReceiver the will offer to receive RTP.
+	// to send RTP and the RTPReceiver will offer to receive RTP.
 	RTPTransceiverDirectionSendrecv RTPTransceiverDirection = iota + 1
 
 	// RTPTransceiverDirectionSendonly indicates the RTPSender will offer
 	// to send RTP.
 	RTPTransceiverDirectionSendonly
 
-	// RTPTransceiverDirectionRecvonly indicates the RTPReceiver the will
+	// RTPTransceiverDirectionRecvonly indicates the RTPReceiver will
 	// offer to receive RTP.
 	RTPTransceiverDirectionRecvonly
 
 	// RTPTransceiverDirectionInactive indicates the RTPSender won't offer
-	// to send RTP and RTPReceiver the won't offer to receive RTP.
+	// to send RTP and the RTPReceiver won't offer to receive RTP.
 	RTPTransceiverDirectionInactive
 )
 
