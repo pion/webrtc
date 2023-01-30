9d98f238b5a4ad6f851ce04fa9ab7380cbe8f2cd
Benny Daon <benny@tuzig.com>
2023-01-30 17:31:10 +0200

    fix mising vcs information
    when buildign the data channel example

--- ats/data-channels/start_server.bash
+++ ats/data-channels/start_server.bash
@@ -1,7 +1,7 @@
 GO=/usr/local/go/bin/go
 cd "/go/src/github.com/pion/webrtc/examples/data-channels"
 TMP=`mktemp`
-$GO build . > $TMP 2>&1
+$GO build -buildvcs=false . > $TMP 2>&1
 
 if [ $? -eq 0 ]; then
     echo $1 | ./data-channels
