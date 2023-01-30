47c06893d0536136d1ab02fa15fc8804e36ddbbf
Benny Daon <benny@tuzig.com>
2023-01-30 17:04:08 +0200

    Add more detailed error when build fails
    problem is, it's all get squeezed in the answer field..

--- ats/data-channels/start_server.bash
+++ ats/data-channels/start_server.bash
@@ -1,4 +1,11 @@
 GO=/usr/local/go/bin/go
 cd "/go/src/github.com/pion/webrtc/examples/data-channels"
-$GO build . > /dev/null 2>&1
-echo $1 | ./data-channels
+TMP=`mktemp`
+$GO build . > $TMP 2>&1
+
+if [ $? -eq 0 ]; then
+    echo $1 | ./data-channels
+else
+    cat $TMP
+fi
+rm $TMP
