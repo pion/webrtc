b6191eb14b5f9fa5ede917c30cba5c25d3bb3245
Yutaka Takeda <yt0916@gmail.com>
2023-03-26 10:51:33 -0700

    Fix potential deadlock in the example
    Use bufferred channel (size 1) with select{} on write to avoid potential
    deadlock

--- examples/data-channels-flow-control/main.go
+++ examples/data-channels-flow-control/main.go
@@ -52,7 +52,7 @@ func createOfferer() *webrtc.PeerConnection {
 		MaxRetransmits: &maxRetransmits,
 	}
 
-	sendMoreCh := make(chan struct{})
+	sendMoreCh := make(chan struct{}, 1)
 
 	// Create a datachannel with label 'data'
 	dc, err := pc.CreateDataChannel("data", options)
@@ -79,7 +79,12 @@ func createOfferer() *webrtc.PeerConnection {
 
 	// This callback is made when the current bufferedAmount becomes lower than the threshold
 	dc.OnBufferedAmountLow(func() {
-		sendMoreCh <- struct{}{}
+		// Make sure to not block this channel or perform long running operations in this callback
+		// This callback is executed by pion/sctp. If this callback is blocking it will stop operations
+		select {
+		case sendMoreCh <- struct{}{}:
+		default:
+		}
 	})
 
 	return pc
