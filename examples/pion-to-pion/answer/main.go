ce37669ddab8bf4805c7bdffb033a427f527cc56
Sean DuBois <sean@siobud.com>
2024-07-18 15:21:46 -0400

    Delete deadcode in pion-to-pion/answer
    Was accidentally copied, never needed

--- examples/pion-to-pion/answer/main.go
+++ examples/pion-to-pion/answer/main.go
@@ -5,17 +5,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -190,46 +186,4 @@ func main() { // nolint:gocognit
 	// Start HTTP server that accepts requests from the offer process to exchange SDP and Candidates
 	// nolint: gosec
 	panic(http.ListenAndServe(*answerAddr, nil))
-}
-
-// Read from stdin until we get a newline
-func readUntilNewline() (in string) {
-	var err error
-
-	r := bufio.NewReader(os.Stdin)
-	for {
-		in, err = r.ReadString('\n')
-		if err != nil && !errors.Is(err, io.EOF) {
-			panic(err)
-		}
-
-		if in = strings.TrimSpace(in); len(in) > 0 {
-			break
-		}
-	}
-
-	fmt.Println("")
-	return
-}
-
-// JSON encode + base64 a SessionDescription
-func encode(obj *webrtc.SessionDescription) string {
-	b, err := json.Marshal(obj)
-	if err != nil {
-		panic(err)
-	}
-
-	return base64.StdEncoding.EncodeToString(b)
-}
-
-// Decode a base64 and unmarshal JSON into a SessionDescription
-func decode(in string, obj *webrtc.SessionDescription) {
-	b, err := base64.StdEncoding.DecodeString(in)
-	if err != nil {
-		panic(err)
-	}
-
-	if err = json.Unmarshal(b, obj); err != nil {
-		panic(err)
-	}
 }
