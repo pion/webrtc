5cd1cb369aff9eb3611acb73573d959ea552e57d
Eric Daniels <eric@erdaniels.com>
2024-08-12 14:16:39 -0400

    Run ops.GracefulClose earlier in pc.Close

--- peerconnection.go
+++ peerconnection.go
@@ -2123,6 +2123,7 @@ func (pc *PeerConnection) close(shouldGracefullyClose bool) error {
 	}
 	if shouldGracefullyClose && !alreadyGracefullyClosed {
 		defer close(pc.isGracefulClosedDone)
+		pc.ops.GracefulClose()
 	}
 
 	// https://www.w3.org/TR/webrtc/#dom-rtcpeerconnection-close (step #3)
@@ -2179,8 +2180,6 @@ func (pc *PeerConnection) close(shouldGracefullyClose bool) error {
 	pc.updateConnectionState(pc.ICEConnectionState(), pc.dtlsTransport.State())
 
 	if shouldGracefullyClose {
-		pc.ops.GracefulClose()
-
 		// note that it isn't canon to stop gracefully
 		pc.sctpTransport.lock.Lock()
 		for _, d := range pc.sctpTransport.dataChannels {
