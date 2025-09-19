75f50cb59e0b66cac83716c785bdc641faf810c6
Sean DuBois <sean@pion.ly>
2025-09-19 16:41:51 -0400

    Update TestPeerConnection_SessionID to run on WASM
    Relates to #2714

--- peerconnection_test.go
+++ peerconnection_test.go
@@ -761,3 +761,48 @@ func TestDTLSClose(t *testing.T) {
 	peerConnectionsConnected.Wait()
 	assert.NoError(t, pcOffer.Close())
 }
+
+func TestPeerConnection_SessionID(t *testing.T) {
+	defer test.TimeOut(time.Second * 10).Stop()
+	defer test.CheckRoutines(t)()
+
+	pcOffer, pcAnswer, err := newPair()
+	assert.NoError(t, err)
+	var offerSessionID uint64
+	var offerSessionVersion uint64
+	var answerSessionID uint64
+	var answerSessionVersion uint64
+	for i := 0; i < 10; i++ {
+		assert.NoError(t, signalPair(pcOffer, pcAnswer))
+		var offer sdp.SessionDescription
+		assert.NoError(t, offer.UnmarshalString(pcOffer.LocalDescription().SDP))
+
+		sessionID := offer.Origin.SessionID
+		sessionVersion := offer.Origin.SessionVersion
+
+		if offerSessionID == 0 {
+			offerSessionID = sessionID
+			offerSessionVersion = sessionVersion
+		} else {
+			assert.Equalf(t, offerSessionID, sessionID, "offer[%v] session id mismatch", i)
+			assert.Equalf(t, offerSessionVersion+1, sessionVersion, "offer[%v] session version mismatch", i)
+			offerSessionVersion++
+		}
+
+		var answer sdp.SessionDescription
+		assert.NoError(t, offer.UnmarshalString(pcAnswer.LocalDescription().SDP))
+
+		sessionID = answer.Origin.SessionID
+		sessionVersion = answer.Origin.SessionVersion
+
+		if answerSessionID == 0 {
+			answerSessionID = sessionID
+			answerSessionVersion = sessionVersion
+		} else {
+			assert.Equalf(t, answerSessionID, sessionID, "answer[%v] session id mismatch", i)
+			assert.Equalf(t, answerSessionVersion+1, sessionVersion, "answer[%v] session version mismatch", i)
+			answerSessionVersion++
+		}
+	}
+	closePairNow(t, pcOffer, pcAnswer)
+}
